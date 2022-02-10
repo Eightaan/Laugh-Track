4cea589d8fc138384729222bd7ead24b6d752414
Eightaan <57832730+Eightaan@users.noreply.github.com>
2022-02-10 18:25:22 +0100

    Add files via upload

--- Laugh Track/lua/LaughTrack.lua
+++ Laugh Track/lua/LaughTrack.lua
@@ -4,17 +4,23 @@ end
 
 blt.xaudio.setup()
 
-local custody_track = LaughTrack:GetOption("custody_laugh_track") > 1 and "Sound/cardi_b_laugh.ogg" or "Sound/LaughTrack.ogg"
-local downed_track = LaughTrack:GetOption("downed_laugh_track") > 1 and "Sound/cardi_b_laugh.ogg" or "Sound/LaughTrack.ogg"
-local cuffed_track = LaughTrack:GetOption("cuffed_laugh_track") > 1 and "Sound/cardi_b_laugh.ogg" or "Sound/LaughTrack.ogg"
-local tased_track = LaughTrack:GetOption("tased_laugh_track") > 1 and "Sound/cardi_b_laugh.ogg" or "Sound/LaughTrack.ogg"
+local tased_setting = LaughTrack:GetOption("tased_laugh_track")
+local downed_setting = LaughTrack:GetOption("downed_laugh_track")
+local cuffed_setting = LaughTrack:GetOption("cuffed_laugh_track")
+local custody_setting = LaughTrack:GetOption("custody_laugh_track")
+
 local theme_track = LaughTrack:GetOption("theme_track") and "Sound/theme.ogg" or "Sound/nothing.ogg"
+local tased_track = tased_setting < 2 and "sound/LaughTrack.ogg" or tased_setting > 2 and "Sound/curb.ogg" or "Sound/cardi_b_laugh.ogg"
+local downed_track = downed_setting < 2 and "Sound/LaughTrack.ogg" or downed_setting > 2 and "Sound/curb.ogg" or "Sound/cardi_b_laugh.ogg"
+local cuffed_track = cuffed_setting < 2 and "Sound/LaughTrack.ogg" or cuffed_setting > 2 and "Sound/curb.ogg" or "Sound/cardi_b_laugh.ogg"
+local custody_track = custody_setting < 2 and "Sound/LaughTrack.ogg" or custody_setting > 2 and "Sound/curb.ogg" or "Sound/cardi_b_laugh.ogg"
+
+LaughTrack.nothing = XAudio.Buffer:new(ModPath .. "Sound/nothing.ogg")
 LaughTrack.custody = XAudio.Buffer:new(ModPath .. custody_track)
 LaughTrack.downed = XAudio.Buffer:new(ModPath .. downed_track)
-LaughTrack.tazed = XAudio.Buffer:new(ModPath .. tased_track)
 LaughTrack.cuffed = XAudio.Buffer:new(ModPath .. cuffed_track)
+LaughTrack.tazed = XAudio.Buffer:new(ModPath .. tased_track)
 LaughTrack.theme = XAudio.Buffer:new(ModPath .. theme_track)
-LaughTrack.nothing = XAudio.Buffer:new(ModPath .. "Sound/nothing.ogg")
 
 if RequiredScript == "lib/managers/hud/hudplayercustody" then
     local lastplayed
@@ -25,7 +31,7 @@ if RequiredScript == "lib/managers/hud/hudplayercustody" then
                 lastplayed = Application:time()
                 local laugh = XAudio.Source:new(LaughTrack.custody)
 				local RNG = math.random(0,100)
-				local theme = RNG < 10 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
+				local theme = RNG < 5 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
                 laugh:set_relative(true)
 				theme:set_relative(true)
 			    laugh:set_type(XAudio.Source.SOUND_EFFECT)
@@ -43,7 +49,7 @@ elseif RequiredScript == "lib/units/beings/player/playerdamage" then
                 lastplayed = Application:time()
                 local laugh = XAudio.Source:new(LaughTrack.downed)
 				local RNG = math.random(0,100)
-				local theme = RNG < 10 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
+				local theme = RNG < 5 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
                 laugh:set_relative(true)
 				theme:set_relative(true)
 				laugh:set_type(XAudio.Source.SOUND_EFFECT)
@@ -59,7 +65,7 @@ elseif RequiredScript == "lib/units/beings/player/playerdamage" then
                 lastplayed = Application:time()
                 local laugh = XAudio.Source:new(LaughTrack.tazed)
 				local RNG = math.random(0,100)
-				local theme = RNG < 10 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
+				local theme = RNG < 5 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
                 laugh:set_relative(true)
 				theme:set_relative(true)
 			    laugh:set_type(XAudio.Source.SOUND_EFFECT)
@@ -75,7 +81,7 @@ elseif RequiredScript == "lib/units/beings/player/playerdamage" then
                 lastplayed = Application:time()
                 local laugh = XAudio.Source:new(LaughTrack.cuffed)
 				local RNG = math.random(0,100)
-				local theme = RNG < 10 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
+				local theme = RNG < 5 and XAudio.Source:new(LaughTrack.theme) or XAudio.Source:new(LaughTrack.nothing)
                 laugh:set_relative(true)
 				theme:set_relative(true)
 			    laugh:set_type(XAudio.Source.SOUND_EFFECT)
